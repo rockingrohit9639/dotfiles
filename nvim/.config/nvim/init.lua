471ba369f37ef218eb9554eef88cc7eae32abbf8
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-07 21:10:37 +0530

    feat(nvim): clean init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
+require("vim-options")
 require("config.lazy")
 
--- setting up colortheme
-vim.cmd.colorscheme "catppuccin-mocha"
 
--- setting up telescope
-local builtin = require('telescope.builtin')
-
--- setting up shortcuts for telescope
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- setting up treesitter
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-  ensure_installed = { "c", "lua", "vim", "javascript", "typescript", "go", "html" },
-  highlight = { enable = true },
-  indent = { enable = true },  
-})
---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = {},
+      },
+    }
+  },
+  config = function() 
+    vim.keymap.set('n', '<C-e>', ':Neotree filesystem reveal right<CR>', {})
+  end
+}
