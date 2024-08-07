2ef08ff47a03eb935fc067ff9399a62238a26ebd
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-07 19:55:28 +0530

    feat(nvim): add treesitter plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,13 +6,20 @@ vim.g.mapleader = " "
 
 require("config.lazy")
 
--- initializing colortheme
+-- setting up colortheme
 vim.cmd.colorscheme "catppuccin-mocha"
 
--- initializing telescope
+-- setting up telescope
 local builtin = require('telescope.builtin')
 
 -- setting up shortcuts for telescope
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
+-- setting up treesitter
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "javascript", "typescript", "go", "html" },
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
