813c05a913f3f7b6405fd87e2a7b0af1fcb94129
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-07 10:08:50 +0530

    feat(nvim): add catppuccin and telescope plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,5 +2,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 
 require("config.lazy")
+
+-- initializing colortheme
+vim.cmd.colorscheme "catppuccin-mocha"
+
+-- initializing telescope
+local builtin = require('telescope.builtin')
+
+-- setting up shortcuts for telescope
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
