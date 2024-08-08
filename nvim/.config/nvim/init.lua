71d5c44f19ac5c7c959058c849c4e8e1ea06996a
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-08 23:14:58 +0530

    feat(nvim): setup auto-completions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,5 @@
+vim.opt.relativenumber = true
+
 require("vim-options")
 require("config.lazy")
 require("fuzzy")
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,44 +2,86 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require('mason').setup()
-    end
+      require("mason").setup()
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true
+      auto_install = true,
     },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "astro", "bashls", "ast_grep", "dockerls", "gopls", "html", "biome", "harper_ls", "prismals", "sqlls", "grammarly" }
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "astro",
+          "bashls",
+          "ast_grep",
+          "dockerls",
+          "gopls",
+          "html",
+          "biome",
+          "harper_ls",
+          "prismals",
+          "sqlls",
+          "grammarly",
+        },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.astro.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.ast_grep.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.biome.setup({})
-      lspconfig.harper_ls.setup({})
-      lspconfig.prismals.setup({})
-      lspconfig.sqlls.setup({})
-      lspconfig.grammarly.setup({})
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.astro.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ast_grep.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.biome.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.harper_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.prismals.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.sqlls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.grammarly.setup({
+        capabilities = capabilities,
+      })
 
       -- Setting up shortcut keys for lsp
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<C-.>", vim.lsp.buf.code_action, {})
-    end
-  }
+    end,
+  },
 }
