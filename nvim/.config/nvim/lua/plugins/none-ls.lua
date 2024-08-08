54edccddc03e015278d52775e274907bf80e5eb9
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-08 23:32:46 +0530

    feat(none-ls): enable format on save

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,35 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    -- Setting up formatting
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
-      },
-    })
+		-- Setting up formatting
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+			},
+			on_attach = function(client, bufnr)
+				-- Formatting on save
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
 
-    -- Shortcut key for formatting
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		-- Shortcut key for formatting
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
