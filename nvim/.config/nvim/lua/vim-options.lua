8641dfa2267e6feaa7eccaf86e3241d58bd7940b
rockingrohit9639 <rohitbhatwara1@gmail.com>
2024-08-23 12:18:02 +0530

    feat(vim): set default vim options

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,5 +2,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.smarttab = true
