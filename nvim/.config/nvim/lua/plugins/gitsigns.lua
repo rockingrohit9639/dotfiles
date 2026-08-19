5d24aa1d70abc397d67452d3e4ee1c4d719ac5a6
rockingrohit9639 <rohitbhatwara1@gmail.com>
2026-08-19 11:00:39 +0200

    feat(nvim): show inline git blame on the current line
    Turn on gitsigns current_line_blame so the cursor line gets GitLens-style
    virtual text, and bind <leader>gb to blame_line for the full commit.

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,19 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
+        delay = 300,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = "  <author>, <author_time:%R> · <summary>",
+    })
 
     vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
     vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
   end,
 }
